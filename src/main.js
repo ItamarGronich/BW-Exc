6ad1dc0c1d6a31d2a9e7aa5838ad015fe55a61e2
ItamarGronich <ItamarGronich@Gmail.com>
2017-04-07 23:48:12 +0300

    Adding basic modules structure.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,65 @@
-import Menu from './menu.js';
-import Language from './language.js';
-import Youtube from './youtube.js';
+/**
+ * Main DevTools module.
+ *
+ * Holds the dom model, instantiate the DevTools, and connects all the
+ * functionality.
+ */
+const DevTools = function () {
 
+  /** The DOM model module. Holds the structure of the Dom and will be used to
+   *  generate the DevTools dom representation.
+   *
+   *  @method addElement
+   *  @method remove
+   */
+  const DomModelFactory = function () {
+
+    // The DOM model map. Will hold the structure of the DOM.
+    let DomModel = new Map();
+
+    /*  ===  Private Methods ===  */
+    function updateModle() {
+
+    }
+
+    /*  ===  Public Methods ===  */
+    return {
+      addElement() {
+
+      },
+
+      removeElement() {
+
+      },
+
+      getModel() {
+
+      }gca 
+    }
+
+  };
+
+  return {
+    DomModel: DomModelFactory()
+  }
+};
+
+/**
+ * DevTools Html element wrapper.
+ */
+class Element {
+
+  constructor(elem) {
+    this.DomNode = elem;
+  }
+
+
+}
 
 // HTML fully parsed. Dom now safely editable.
 document.addEventListener('DOMContentLoaded', () => {
 
-  // Enable language support. Translate page.
-  new Language().translate();
-
-  // Initiate Menu.
-  new Menu();
-
-  // Initiate Youtube handler module.
-  new Youtube();
+  DevTools();
 
 });
 
