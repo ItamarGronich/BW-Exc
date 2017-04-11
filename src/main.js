ed088e3d82794d350fa4da62c42efca84df7d542
ItamarGronich <ItamarGronich@Gmail.com>
2017-04-11 09:26:31 +0300

    Get animation for left, right and center dropping zones

--- src/main.js
+++ src/main.js
@@ -6,20 +6,8 @@
  */
 const DevTools = function () {
 
-  class Utils {
-
-    // Generates a numerical hash from a string.
-    static hashCode(string) {
-      let hash = 0, i, chr;
-      if (string.length === 0) return hash;
-      for (i = 0; i < string.length; i++) {
-        chr  = string.charCodeAt(i);
-        hash = ((hash << 5) - hash) + chr;
-        hash |= 0; // Convert to 32bit integer
-      }
-      return hash;
-    };
-  }
+  // Will hold reference to the element being dragged.
+  let dragged;
 
   const
 
@@ -36,7 +24,7 @@ const DevTools = function () {
     mouseout      = 'mouseout',
     dragstart     = 'dragstart',
     drop          = 'drop',
-    dragenter     = 'dragenter',
+    dragover      = 'dragover',
     dragleave     = 'dragleave',
 
     /**
@@ -45,35 +33,56 @@ const DevTools = function () {
      * @param model
      */
     view          = (model) => {
-      class Element {
 
-        /**
-         * Construct the element object.
-         *
-         * @param {HTMLElement} el
-         */
-        constructor(el, parent) {
+      const buildTree = (model, parent) => {
 
-          // Original node.
-          this.node = el;
+        if ( model === devToolsFrame
+          || model.nodeType !== Node.ELEMENT_NODE
+          || model.tagName.toLowerCase() === 'script' ) {
+          return;
+        }
+
+        let el = new Element(model, parent);
 
 
-          // Twin node in devtools.
-          this.twin = document.createElement('div');
+        if ( model.hasChildNodes() ) {
+          Array.from(model.childNodes).forEach(node => buildTree(node, el));
+        }
 
-          // Attach hover events to the twin element.
-          [mouseover, mouseout].forEach(eventName =>
-            this.twin.addEventListener(eventName, e => this.hover(event))
-          );
+      };
 
-          // Attach darg and drop events to the twin element.
-          [dragstart, drop, dragenter, dragleave].forEach(eventName =>
-            this.twin.addEventListener(eventName, e => this.drag(event), false)
-          );
+      buildTree(model, null);
 
-          this.twin.textContent = el.tagName.toLowerCase();
-          this.twin.setAttribute('style',
-            `min-width: 20px;
+    };
+
+  class Element {
+
+    /**
+     * Construct the element object.
+     *
+     * @param {HTMLElement} el
+     */
+    constructor(el, parent) {
+
+      // Original node.
+      this.node = el;
+
+      // Twin node in devtools.
+      this.twin = document.createElement('div');
+
+      // Attach hover events to the twin element.
+      [mouseover, mouseout].forEach(eventName =>
+        this.twin.addEventListener(eventName, e => this.hover(event))
+      );
+
+      // Attach darg and drop events to the twin element.
+      [dragstart, drop, dragover, dragleave].forEach(eventName =>
+        this.twin.addEventListener(eventName, e => this.drag(event), false)
+      );
+
+      this.twin.textContent = el.tagName.toLowerCase();
+      this.twin.setAttribute('style',
+        `min-width: 20px;
              min-height: 20px;
              border: 1px solid black;
              display: inline-block;
@@ -85,89 +94,121 @@ const DevTools = function () {
              transition-delay: 100ms;
              background: ${this.generateColor()}`);
 
-          this.twin.setAttribute('draggable', 'true');
-          if (!parent) {
-            parent = devToolsFrame;
-          }
-
-          this.appendEl(parent);
-
-        }
-
-        hover(event) {
-
-          switch (event.type) {
-            case mouseover:
-              this.node.style.background = 'rgba(35,240,60,.25)';
-              if (event.target === this.twin) {
-                event.target.style.background = this.generateColor('60%', '70%');
-              }
-
-              break;
-            case mouseout:
-              this.node.style.background = '';
-              if (event.target === this.twin) {
-                event.target.style.background = this.generateColor();
-              }
-              break;
-          }
-        }
-
-        drag(event) {
-
-          switch (event.type) {
-            case dragstart:
-              console.log(dragstart);
-              
-              break;
-            case dragenter:
-              console.log(dragenter);
-
-              break;
-              case dragleave:
-                console.log(dragleave);
-
-              break;
-          }
-
-        }
-
-        appendEl(parent) {
-          if (parent instanceof Element) {
-            parent.twin.appendChild(this.twin)
-          } else {
-            parent.appendChild(this.twin);
-          }
-        }
-
-        generateColor(saturation = '60%', luminosity = '60%') {
-          let hue = Utils.hashCode(this.node.tagName.toLowerCase());
-          return `hsl(${hue}, ${saturation}, ${luminosity})`;
-        }
+      this.twin.setAttribute('draggable', 'true');
+      if (!parent) {
+        parent = devToolsFrame;
       }
 
-      const buildTree = (model, parent) => {
+      this.appendEl(parent);
 
-        if (model === devToolsFrame
-          || model.nodeType !== Node.ELEMENT_NODE
-          || model.tagName.toLowerCase() === 'script') {
-          return;
-        }
+    }
 
-        let el;
+    hover(event) {
 
-        el = new Element(model, parent);
+      switch (event.type) {
+        case mouseover:
+          this.node.style.background = 'rgba(35,240,60,.25)';
+          if (event.target === this.twin) {
+            event.target.style.background = this.generateColor('60%', '70%');
+          }
+
+          break;
+        case mouseout:
+          this.node.style.background = '';
+          if (event.target === this.twin) {
+            event.target.style.background = this.generateColor();
+          }
+          break;
+      }
+    }
+
+    drag(event) {
+      event.stopPropagation();
+
+      switch (event.type) {
+        case dragstart:
+
+          // Store the element being dragged.
+          dragged = event.target;
+          event.dataTransfer.dropEffect = 'move';
+
+          break;
+        case dragover:
+
+          if ( event.target === dragged ) return;
+
+          event.preventDefault();
+
+          const
+            hoverPoint = event.x,
+            clientRect = event.target.getBoundingClientRect(),
+            getSection = (x, {right, left}) => {
+
+              // 10% of the width of the element. limited to min 5 and max 20;
+              const edgeArea = Math.max(Math.min(event.target.clientWidth * 0.1, 5), 20);
+
+              // Left or right if currently in 10% range of the element width.
+              if ( x > left && x < (left + edgeArea) ) {
+                return 'left';
+              } else if (x < right && x > (right - edgeArea)) {
+                return 'right';
+              }
+            };
+
+          switch (getSection(hoverPoint, clientRect)) {
+            case 'left':
+              event.target.style.border = '1px solid black';
+              event.target.style.borderLeft = '4px solid black';
+              break;
+            case 'right':
+              event.target.style.border = '1px solid black';
+              event.target.style.borderRight = '4px solid black';
+              break;
+            default:
+              event.target.style.border = '1px solid black';
+              event.target.style.border = '4px solid black';
+              break;
+          }
 
 
-        if (model.hasChildNodes()) {
-          Array.from(model.childNodes).forEach(node => buildTree(node, el));
-        }
+          break;
+        case dragleave:
+          console.log(dragleave);
+          event.target.style.border = '1px solid black';
 
-      };
+          break;
+      }
 
-      buildTree(model, null);
+    }
 
+    appendEl(parent) {
+      if (parent instanceof Element) {
+        parent.twin.appendChild(this.twin)
+      } else {
+        parent.appendChild(this.twin);
+      }
+    }
+
+    generateColor(saturation = '60%', luminosity = '60%') {
+      let hue = Utils.hashCode(this.node.tagName.toLowerCase());
+      return `hsl(${hue}, ${saturation}, ${luminosity})`;
+    }
+  }
+
+  class Utils {
+
+    // Generates a numerical hash from a string.
+    static hashCode(string) {
+      let hash = 0, i, chr;
+      if (string.length === 0) return hash;
+      for (i = 0; i < string.length; i++) {
+        chr  = string.charCodeAt(i);
+        hash = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+      }
+      return hash;
     };
+  }
 
   return {
 
